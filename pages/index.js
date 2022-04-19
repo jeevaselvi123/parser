f1bfc9d8dba8b507f4b313e10f1fed2dbe1f35df
Jeeva Dharani <jeevaselvi1999@gmail.com>
2022-04-19 23:35:26 +0530

    Revert "Modified the grammar and updated the UI"
    This reverts commit aaa499ff556cadcbcce817440751d900a2154e6e.

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
-import {
-  EmojiHappyIcon,
-  EmojiSadIcon,
-  SearchIcon,
-} from "@heroicons/react/solid";
-import Nearley from "nearley";
-import { useState } from "react";
-import grammar from "../Parser/foodFilter";
+import { SearchIcon } from "@heroicons/react/solid";
+import axios from "axios";
+import nearley from "nearley";
+import { useEffect, useState } from "react";
+import grammar from "../carparser";
 
 export default function Home() {
   const [value, setValue] = useState();
   const [status, setStatus] = useState(false);
+  const [temp, setTemp] = useState();
+  let result;
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/cars`).then((res) => setTemp(res.data));
+    // console.log(temp);
+  }, []);
 
   const setClick = () => {
-    const parser = new Nearley.Parser(Nearley.Grammar.fromCompiled(grammar));
-
+    console.log(value);
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     try {
       parser.feed(value);
-      var parsedData = JSON.stringify(parser.results);
-      console.log(parsedData);
-      if (parsedData && parsedData[0] === null && parsedData.length === 0)
-        setStatus(false);
-      else if (parsedData && parsedData[0] && parsedData[0].length > 0)
-        setStatus(true);
+      console.log(JSON.stringify(parser.results));
+      alert(parser.results);
+      result = parser.results;
+      console.log(result);
     } catch (parseError) {
       console.log("Error at character " + parseError.message);
-      alert("Entered wrong input.Valid format key:value pair");
-      setStatus(false);
     }
   };
   return (
@@ -33,31 +33,20 @@ export default function Home() {
       <div className="flex min-h-full w-full justify-center p-8">
         <div className="m-10 flex flex-row justify-between">
           <div className="basis-1/12">
-            <div className="w-screen rounded-3xl bg-white shadow-lg text-center">
+            <div className="w-screen rounded-lg border-2 bg-white shadow-lg text-center">
               <p className="pt-2 font-semibold text-2xl">
-                Filter the food items based on the specific category
+                Search for the required details
               </p>
-              <div className=" pt-6 pl-4 justify-center">
-                <div className=" flex flex-row justify-center">
-                  Search <SearchIcon className="w-5 h-5 m-1"></SearchIcon>
-                </div>
-                <div className="flex items-center pr-8">
-                  {status && (
-                    <EmojiHappyIcon className="w-10 h-10 fill-green-600"></EmojiHappyIcon>
-                  )}
-                  {!status && (
-                    <EmojiSadIcon className="w-10 h-10 fill-red-600 animate-bounce"></EmojiSadIcon>
-                  )}
-                  <input
-                    onChange={(e) =>
-                      setValue((prevValue) => (prevValue = e.target.value))
-                    }
-                    placeholder="Type the food category followed by food item ie.Beverage : Kesar"
-                    id="inputField"
-                    className="m-4 w-[100%] border-2 border-black text-center"
-                    type="text"
-                  />
-                </div>
+              <div className=" pt-6 pl-4">
+                <SearchIcon className="w-5 h-5"></SearchIcon>Search:
+                <input
+                  onChange={(e) =>
+                    setValue((prevValue) => (prevValue = e.target.value))
+                  }
+                  id="inputField"
+                  className="m-4 w-[50%] border-2 border-x border-black"
+                  type="text"
+                />
               </div>
               <div className="p-4">
                 <button
@@ -67,6 +56,7 @@ export default function Home() {
                   Parse String
                 </button>
               </div>
+              {result != null && <p className="text-xl">{result[0]}</p>}
             </div>
           </div>
         </div>
